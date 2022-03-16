914661a7833fd045ab3d3c005b37550c2eeda9fb
Vivek Prakash <prakashvivek444@gmail.com>
2022-03-17 00:25:27 +0530

    Added middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 mongoose.connect(process.env.DATABASE, 
 { useNewUrlParser: true,
@@ -13,6 +17,11 @@ mongoose.connect(process.env.DATABASE,
 //catch(
 //    console.log('DB got OOOPS')
 //);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
 
 const port = process.env.PORT || 8000;
 
